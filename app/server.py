bbae1897a5e636c2dfbf1aaee7e3f98f37b14491
Fernandocarvalhaes007 <fernandosantos07@gmail.com>
2024-03-04 01:36:02 -0300

    feat: pequena melhorias no codigo

--- app/server.py
+++ app/server.py
@@ -1,3 +1,4 @@
+# server.py
 from flask import Flask, request
 from processFile import process_upload_data
 
@@ -6,19 +7,21 @@ app = Flask(__name__)
 @app.route('/upload', methods=['POST'])
 def upload():
     try:
-
-        file = request.files['data']
         
         if 'data' not in request.files:
             return 'Nenhum arquivo enviado', 400
 
+        file = request.files['data']
+    
         file_lines = [line.decode('utf-8') for line in file]
+
+    
         result = process_upload_data(file_lines)
-        return result , 200
+        return result, 200
 
     except Exception as e:
         print(f"Erro ao processar o arquivo enviado: {e}")
         return "Ocorreu um erro ao processar o arquivo enviado", 500
 
 if __name__ == "__main__":
- app.run(port=5000, host='localhost', debug=True)
+    app.run(debug=True)
